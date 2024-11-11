36d13bc64c5301693490d5d2f136b5e6face5def
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-11 13:47:13 +0600

    Displayed AddTask & TodoList components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import TodoList from './Containers/TodoList/TodoList.tsx';
+import AddTask from './Containers/AddTask/AddTask.tsx';
 
 const App = () => {
-  return <></>;
+  return <div className="container m-5 d-flex flex-column align-items-center">
+    <AddTask/>
+    <TodoList/>
+  </div>;
 };
 
 export default App;

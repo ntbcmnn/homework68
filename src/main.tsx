f15bf6d74662021cc8075138436a7c5394cba2e1
Natalya Botsman <botsmannatashaa@gmail.com>
2024-11-11 13:28:19 +0600

    Added react-bootstrap & react-toastify deps

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
 import { store } from './app/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
+    <ToastContainer/>
     <App/>
   </Provider>,
 );
